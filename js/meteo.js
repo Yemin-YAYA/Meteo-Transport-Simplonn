df05379efa7cd4390038eac387c0b7b5e9cd0282
Yaya Yemin <yaya.yemin@laposte.net>
2025-11-21 12:01:57 +0100

    Mise en forme du code
    Mise en forme du code via l'extension installer sur VScode

--- js/meteo.js
+++ js/meteo.js
@@ -1,13 +1,13 @@
 
 // Fonction pour récupérer la météo depuis L'Api
 function getWeather(ville) {
-    const apiKey = "b427e47dd6044708852173249252703";    
+    const apiKey = "b427e47dd6044708852173249252703";
     const url = `https://api.weatherapi.com/v1/current.json?key=${apiKey}&q=${ville}&lang=fr`;
-    
+
     fetch(url)
-    .then(response => response.json())
-    .then(data => displayWeather(data))
-    .catch(error => console.error("Erreur lors de l'appel de l'API :", error));
+        .then(response => response.json())
+        .then(data => displayWeather(data))
+        .catch(error => console.error("Erreur lors de l'appel de l'API :", error));
 }
 
 //Fonction qui affiche les données 
@@ -16,11 +16,11 @@ function displayWeather(data) {
     document.querySelector(".temp").innerText = data.current.temp_c + "°C";
     document.querySelector(".humidity").innerText = "Humidité : " + data.current.humidity + "%";
     document.querySelector(".wind").innerText = "Vent : " + data.current.wind_kph + " km/h";
-     const iconUrl = "https:" + data.current.condition.icon;
+    const iconUrl = "https:" + data.current.condition.icon;
     document.querySelector(".weather-icon").src = iconUrl;
     document.querySelector(".description").innerText = data.current.condition.text;
-    document.getElementById("update-time").textContent = "Dernière mise à jour : " + new Date().toLocaleTimeString(); 
-  
+    document.getElementById("update-time").textContent = "Dernière mise à jour : " + new Date().toLocaleTimeString();
+
 }
 
 
@@ -28,14 +28,14 @@ function displayWeather(data) {
 // Lecture du fichier conf.json pour connaître la ville à afficher.
 fetch("conf.json")
     .then(response => response.json())
-   .then(config => {
+    .then(config => {
         const ville = config.ville;
         getWeather(ville);
 
- // Rafraîchissement auto au bout de 1 heure
+        // Rafraîchissement auto au bout de 1 heure
         setInterval(() => {
-          console.log("Actualisation automatique :", new Date().toLocaleTimeString());
-         getWeather(ville);
-    }, 3600000); // 1 heure = 3600000 millisecondes
-   })   
-   .catch(error => console.error ("Erreur lors du chargement de la configuration :", error));
+            console.log("Actualisation automatique :", new Date().toLocaleTimeString());
+            getWeather(ville);
+        }, 3600000); // 1 heure = 3600000 millisecondes
+    })
+    .catch(error => console.error("Erreur lors du chargement de la configuration :", error));
